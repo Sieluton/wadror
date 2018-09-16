a60da4c9a24ce2f907f11411b5288c44d6e6428e
Sieluton <sami.rinksam@gmail.com>
2018-09-16 23:40:10 +0300

    Tehtävät 6 asti

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,8 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    return "Beer has #{ratings.count} #{"rating".pluralize(ratings.count)}, average #{ratings.average(:score)}"
+  end
 end
