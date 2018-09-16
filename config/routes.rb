1e8cfb6e330e6facfa838496baa2abb9a1a89a9e
Sieluton <sami.rinksam@gmail.com>
2018-09-16 21:25:25 +0300

    Tehtävät 1 ja 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'ratings', to: 'ratings#index'
+  get 'kaikki_bisset', to: 'beers#index'
+  root 'breweries#index'
   resources :beers
   resources :breweries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
