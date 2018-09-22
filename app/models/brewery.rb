046b9015d864988025f0fffd7ebdbd1758821a8d
Sieluton <sami.rinksam@gmail.com>
2018-09-23 01:35:16 +0300

    Tehtävä 8

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -4,5 +4,5 @@ class Brewery < ApplicationRecord
   include RatingAverage
 
   validates :name, presence: true
-  validates :year, inclusion: { in: 1040..2018 }
+  validates :year, numericality: { only_integer: true, greater_than_or_equal_to: 1040, less_than_or_equal_to: proc { Time.now.year } }
 end
