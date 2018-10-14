4f875715fe291c9c5063f525a64855a50ffad708
Sieluton <sami.rinksam@gmail.com>
2018-10-14 09:18:18 +0300

    Tehtävä 10

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,9 @@ Rails.application.routes.draw do
   resources :places, only: [:index, :show]
   post 'places', to:'places#search'
 
+  resources :breweries do
+    post 'toggle_activity', on: :member
+  end
+
   root 'breweries#index'
 end
